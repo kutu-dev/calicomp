4b1eafbdaed4dc195d9421ab6d171a2535f0a5af
Kutu <git@dobon.dev>
2023-11-05 04:14:52 +0100

    Add configs for dunst, fish, Hyprland, Kitty, Neofetch, Starship, Waybar and user-dirs

---
+++ fish/.config/fish/functions/ls.fish
@@ -0,0 +1,3 @@
+function ls -w eza --description "Change the default 'ls' to a configured 'eza'"
+    command eza --icons --git $argv
+end
---
+++ fish/.config/fish/functions/tree.fish
@@ -0,0 +1,3 @@
+function tree -w eza --description "Change the default 'ls' with a configured 'exa' in tree mode"
+    command eza --icons --git --tree $argv
+end
