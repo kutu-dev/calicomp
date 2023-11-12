184d2a464e99343756dfd5d71b1f95f13c5fccfa
Kutu <git@dobon.dev>
2023-11-13 00:08:49 +0100

    Fix unknown command error in the fish sudo function

--- fish/.config/fish/functions/sudo.fish
+++ fish/.config/fish/functions/sudo.fish
@@ -1,7 +1,7 @@
 function sudo -w sudo --description 'Replacement for Bash "sudo !!" command to run the last command with sudo'
     if test "$argv" = !!
         printf %s\\n "sudo $history[1]"
-        command sudo $history[1]
+        command sudo $(string split " " $history[1])
     else
         command sudo $argv
     end
